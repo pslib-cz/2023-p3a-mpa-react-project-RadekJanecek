e1d10b3e691fb7aa7228b72a2976fd71e674863f
radek.janecek.021 <radek.janecek.021@pslib.cz>
2024-03-29 09:15:51 +0100

    Back buttons

--- 6bere/src/components/Game.tsx
+++ 6bere/src/components/Game.tsx
@@ -2,6 +2,9 @@ export const Game = () => {
   return (
     <div>
       <h1>Game</h1>
+      <div>
+        <a href="/">Zpět</a>
+      </div>
     </div>
   );
 }
--- 6bere/src/components/Rules.tsx
+++ 6bere/src/components/Rules.tsx
@@ -2,6 +2,9 @@ export const Rules = () => {
   return (
     <div>
       <h1>Pravidla</h1>
+      <div>
+        <a href="/">Zpět</a>
+      </div>
     </div>
   );
 };
