2370e978e563f6460a24bd84a945fd14a1440f3f
Radek Janeček <radekjanecek123@seznam.cz>
2024-04-16 20:08:13 +0200

    broken auto shuffle

--- 6bere/src/components/Game.tsx
+++ 6bere/src/components/Game.tsx
@@ -1,17 +1,20 @@
 import { Link } from 'react-router-dom';
 import styles from './Game.module.css';
-import { useContext} from 'react';
+import { useContext, useEffect} from 'react';
 import { Context } from '../providers/ContextProvider.tsx';
 import Card from './Card.tsx';
 
 export const Game = () => {
   const context = useContext(Context);
   const player = context.state.players.find(player => player.id === 1);
+
+  useEffect(() => {
+    context.dispatch({ type: 'DEAL_CARDS' });
+  }, []);
   return (
     <div>
       <h1>Game</h1>
       <div>
-        <button onClick={() => {context.dispatch({type: 'DEAL_CARDS'}); console.log(context)}}>Deal cards</button>
         <div className={styles["cards"]}>
           {player && player.cards.map((card) => (
             <Card key={card.id} card={card} />
