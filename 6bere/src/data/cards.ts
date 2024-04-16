87f04f9380ad5771749507d10af0ed386f6c6578
Radek Janeček <radekjanecek123@seznam.cz>
2024-04-16 18:22:26 +0200

    added position to each card

--- 6bere/src/data/cards.ts
+++ 6bere/src/data/cards.ts
@@ -3,418 +3,522 @@ import { CardType } from "../providers/ContextProvider"
 export const cards: CardType[] = [
     {
         id: 1,
-        lives: 1
+        lives: 1,
+        position: "0% 0%"
     },
     {
         id: 2,
-        lives: 1
+        lives: 1,
+        position: "-100% 0%"
     },
     {
         id: 3,
-        lives: 1
+        lives: 1,
+        position: "-200% 0%"
     },
     {
         id: 4,
-        lives: 1
+        lives: 1,
+        position: "-300% 0%"
     },
     {
         id: 5,
-        lives: 1
+        lives: 1,
+        position: "-400% 0%"
     },
     {
         id: 6,
-        lives: 1
+        lives: 1,
+        position: "-500% 0%"
     },
     {
         id: 7,
-        lives: 1
+        lives: 1,
+        position: "-600% 0%"
     },
     {
         id: 8,
-        lives: 1
+        lives: 1,
+        position: "-700% 0%"
     },
     {
         id: 9,
-        lives: 1
+        lives: 1,
+        position: "-800% 0%"
     },
     {
         id: 10,
-        lives: 3
+        lives: 3,
+        position: "-900% 0%"
     },
     {
         id: 11,
-        lives: 5
+        lives: 5,
+        position: "0% 10%"
     },
     {
         id: 12,
-        lives: 1
+        lives: 1,
+        position: "-100% 10%"
     },
     {
         id: 13,
-        lives: 1
+        lives: 1,
+        position: "-200% 10%"
     },
     {
         id: 14,
-        lives: 1
+        lives: 1,
+        position: "-300% 10%"
     },
     {
         id: 15,
-        lives: 2
+        lives: 2,
+        position: "-400% 10%"
     },
     {
         id: 16,
-        lives: 1
+        lives: 1,
+        position: "-500% 10%"
     },
     {
         id: 17,
-        lives: 1
+        lives: 1,
+        position: "-600% 10%"
     },
     {
         id: 18,
-        lives: 1
+        lives: 1,
+        position: "-700% 10%"
     },
     {
         id: 19,
-        lives: 1
+        lives: 1,
+        position: "-800% 10%"
     },
     {
         id: 20,
-        lives: 3
+        lives: 3,
+        position: "-900% 10%"
     },
     {
         id: 21,
-        lives: 1
+        lives: 1,
+        position: "0% 20%"
     },
     {
         id: 22,
-        lives: 5
+        lives: 5,
+        position: "-100% 20%"
     },
     {
         id: 23,
-        lives: 1
+        lives: 1,
+        position: "-200% 20%"
     },
     {
         id: 24,
-        lives: 1
+        lives: 1,
+        position: "-300% 20%"
     },
     {
         id: 25,
-        lives: 2
+        lives: 2,
+        position: "-400% 20%"
     },
     {
         id: 26,
-        lives: 1
+        lives: 1,
+        position: "-500% 20%"
     },
     {
         id: 27,
-        lives: 1
+        lives: 1,
+        position: "-600% 20%"
     },
     {
         id: 28,
-        lives: 1
+        lives: 1,
+        position: "-700% 20%"
     },
     {
         id: 29,
-        lives: 1
+        lives: 1,
+        position: "-800% 20%"
     },
     {
         id: 30,
-        lives: 3
+        lives: 3,
+        position: "-900% 20%"
     },
     {
         id: 31,
-        lives: 1
+        lives: 1,
+        position: "0% 30%"
     },
     {
         id: 32,
-        lives: 1
+        lives: 1,
+        position: "-100% 30%"
     },
     {
         id: 33,
-        lives: 5
+        lives: 5,
+        position: "-200% 30%"
     },
     {
         id: 34,
-        lives: 1
+        lives: 1,
+        position: "-300% 30%"
     },
     {
         id: 35,
-        lives: 2
+        lives: 2,
+        position: "-400% 30%"
     },
     {
         id: 36,
-        lives: 1
+        lives: 1,
+        position: "-500% 30%"
     },
     {
         id: 37,
-        lives: 1
+        lives: 1,
+        position: "-600% 30%"
     },
     {
         id: 38,
-        lives: 1
+        lives: 1,
+        position: "-700% 30%"
     },
     {
         id: 39,
-        lives: 1
+        lives: 1,
+        position: "-800% 30%"
     },
     {
         id: 40,
-        lives: 3
+        lives: 3,
+        position: "-900% 30%"
     },
     {
         id: 41,
-        lives: 1
+        lives: 1,
+        position: "0% 40%"
     },
     {
         id: 42,
-        lives: 1
+        lives: 1,
+        position: "-100% 40%"
     },
     {
         id: 43,
-        lives: 1
+        lives: 1,
+        position: "-200% 40%"
     },
     {
         id: 44,
-        lives: 5
+        lives: 5,
+        position: "-300% 40%"
     },
     {
         id: 45,
-        lives: 2
+        lives: 2,
+        position: "-400% 40%"
     },
     {
         id: 46,
-        lives: 1
+        lives: 1,
+        position: "-500% 40%"
     },
     {
         id: 47,
-        lives: 1
+        lives: 1,
+        position: "-600% 40%"
     },
     {
         id: 48,
-        lives: 1
+        lives: 1,
+        position: "-700% 40%"
     },
     {
         id: 49,
-        lives: 1
+        lives: 1,
+        position: "-800% 40%"
     },
     {
         id: 50,
-        lives: 3
+        lives: 3,
+        position: "-900% 40%"
     },
     {
         id: 51,
-        lives: 1
+        lives: 1,
+        position: "0% 50%"
     },
     {
         id: 52,
-        lives: 1
+        lives: 1,
+        position: "-100% 50%"
     },
     {
         id: 53,
-        lives: 1
+        lives: 1,
+        position: "-200% 50%"
     },
     {
         id: 54,
-        lives: 1
+        lives: 1,
+        position: "-300% 50%"
     },
     {
         id: 55,
-        lives: 7
+        lives: 7,
+        position: "-400% 50%"
     },
     {
         id: 56,
-        lives: 1
+        lives: 1,
+        position: "-500% 50%"
     },
     {
         id: 57,
-        lives: 1
+        lives: 1,
+        position: "-600% 50%"
     },
     {
         id: 58,
-        lives: 1
+        lives: 1,
+        position: "-700% 50%"
     },
     {
         id: 59,
-        lives: 1
+        lives: 1,
+        position: "-800% 50%"
     },
     {
         id: 60,
-        lives: 3
+        lives: 3,
+        position: "-900% 50%"
     },
     {
         id: 61,
-        lives: 1
+        lives: 1,
+        position: "0% 60%"
     },
     {
         id: 62,
-        lives: 1
+        lives: 1,
+        position: "-100% 60%"
     },
     {
         id: 63,
-        lives: 1
+        lives: 1,
+        position: "-200% 60%"
     },
     {
         id: 64,
-        lives: 1
+        lives: 1,
+        position: "-300% 60%"
     },
     {
         id: 65,
-        lives: 2
+        lives: 2,
+        position: "-400% 60%"
     },
     {
         id: 66,
-        lives: 5
+        lives: 5,
+        position: "-500% 60%"
     },
     {
         id: 67,
-        lives: 1
+        lives: 1,
+        position: "-600% 60%"
     },
     {
         id: 68,
-        lives: 1
+        lives: 1,
+        position: "-700% 60%"
     },
     {
         id: 69,
-        lives: 1
+        lives: 1,
+        position: "-800% 60%"
     },
     {
         id: 70,
-        lives: 3
+        lives: 3,
+        position: "-900% 60%"
     },
     {
         id: 71,
-        lives: 1
+        lives: 1,
+        position: "0% 70%"
     },
     {
         id: 72,
-        lives: 1
+        lives: 1,
+        position: "-100% 70%"
     },
     {
         id: 73,
-        lives: 1
+        lives: 1,
+        position: "-200% 70%"
     },
     {
         id: 74,
-        lives: 1
+        lives: 1,
+        position: "-300% 70%"
     },
     {
         id: 75,
-        lives: 2
+        lives: 2,
+        position: "-400% 70%"
     },
     {
         id: 76,
-        lives: 1
+        lives: 1,
+        position: "-500% 70%"
     },
     {
         id: 77,
-        lives: 5
+        lives: 5,
+        position: "-600% 70%"
     },
     {
         id: 78,
-        lives: 1
+        lives: 1,
+        position: "-700% 70%"
     },
     {
         id: 79,
-        lives: 1
+        lives: 1,
+        position: "-800% 70%"
     },
     {
         id: 80,
-        lives: 3
+        lives: 3,
+        position: "-900% 70%"
     },
     {
         id: 81,
-        lives: 1
+        lives: 1,
+        position: "0% 80%"
     },
     {
         id: 82,
-        lives: 1
+        lives: 1,
+        position: "-100% 80%"
     },
     {
         id: 83,
-        lives: 1
+        lives: 1,
+        position: "-200% 80%"
     },
     {
         id: 84,
-        lives: 1
+        lives: 1,
+        position: "-300% 80%"
     },
     {
         id: 85,
-        lives: 2
+        lives: 2,
+        position: "-400% 80%"
     },
     {
         id: 86,
-        lives: 1
+        lives: 1,
+        position: "-500% 80%"
     },
     {
         id: 87,
-        lives: 1
+        lives: 1,
+        position: "-600% 80%"
     },
     {
         id: 88,
-        lives: 5
+        lives: 5,
+        position: "-700% 80%"
     },
     {
         id: 89,
-        lives: 1
+        lives: 1,
+        position: "-800% 80%"
     },
     {
         id: 90,
-        lives: 3
+        lives: 3,
+        position: "-900% 80%"
     },
     {
         id: 91,
-        lives: 1
+        lives: 1,
+        position: "0% 90%"
     },
     {
         id: 92,
-        lives: 1
+        lives: 1,
+        position: "-100% 90%"
     },
     {
         id: 93,
-        lives: 1
+        lives: 1,
+        position: "-200% 90%"
     },
     {
         id: 94,
-        lives: 1
+        lives: 1,
+        position: "-300% 90%"
     },
     {
         id: 95,
-        lives: 2
+        lives: 2,
+        position: "-400% 90%"
     },
     {
         id: 96,
-        lives: 1
+        lives: 1,
+        position: "-500% 90%"
     },
     {
         id: 97,
-        lives: 1
+        lives: 1,
+        position: "-600% 90%"
     },
     {
         id: 98,
-        lives: 1
+        lives: 1,
+        position: "-700% 90%"
     },
     {
         id: 99,
-        lives: 5
+        lives: 5,
+        position: "-800% 90%"
     },
     {
         id: 100,
-        lives: 3
+        lives: 3,
+        position: "-900% 90%"
     },
     {
         id: 101,
-        lives: 1
+        lives: 1,
+        position: "0% 100%"
     },
     {
         id: 102,
-        lives: 1
+        lives: 1,
+        position: "-100% 100%"
     },
     {
         id: 103,
-        lives: 1
+        lives: 1,
+        position: "-200% 100%"
     },
     {
         id: 104,
-        lives: 1
+        lives: 1,
+        position: "-300% 100%"
     }
 ]
