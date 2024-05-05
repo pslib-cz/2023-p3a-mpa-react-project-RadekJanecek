41ed1f201c04c218742117c30c6323513b1d47bb
radek.janecek.021 <radek.janecek.021@pslib.cz>
2024-05-05 16:24:36 +0200

    changed cards

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -1,524 +1,628 @@
 import { CardType } from "../providers/ContextProvider"
+import card1 from "/images/1.jpg"
+import card2 from "/images/2.jpg"
+import card3 from "/images/3.jpg"
+import card4 from "/images/4.jpg"
+import card5 from "/images/5.jpg"
+import card6 from "/images/6.jpg"
+import card7 from "/images/7.jpg"
+import card8 from "/images/8.jpg"
+import card9 from "/images/9.jpg"
+import card10 from "/images/10.jpg"
+import card11 from "/images/11.jpg"
+import card12 from "/images/12.jpg"
+import card13 from "/images/13.jpg"
+import card14 from "/images/14.jpg"
+import card15 from "/images/15.jpg"
+import card16 from "/images/16.jpg"
+import card17 from "/images/17.jpg"
+import card18 from "/images/18.jpg"
+import card19 from "/images/19.jpg"
+import card20 from "/images/20.jpg"
+import card21 from "/images/21.jpg"
+import card22 from "/images/22.jpg"
+import card23 from "/images/23.jpg"
+import card24 from "/images/24.jpg"
+import card25 from "/images/25.jpg"
+import card26 from "/images/26.jpg"
+import card27 from "/images/27.jpg"
+import card28 from "/images/28.jpg"
+import card29 from "/images/29.jpg"
+import card30 from "/images/30.jpg"
+import card31 from "/images/31.jpg"
+import card32 from "/images/32.jpg"
+import card33 from "/images/33.jpg"
+import card34 from "/images/34.jpg"
+import card35 from "/images/35.jpg"
+import card36 from "/images/36.jpg"
+import card37 from "/images/37.jpg"
+import card38 from "/images/38.jpg"
+import card39 from "/images/39.jpg"
+import card40 from "/images/40.jpg"
+import card41 from "/images/41.jpg"
+import card42 from "/images/42.jpg"
+import card43 from "/images/43.jpg"
+import card44 from "/images/44.jpg"
+import card45 from "/images/45.jpg"
+import card46 from "/images/46.jpg"
+import card47 from "/images/47.jpg"
+import card48 from "/images/48.jpg"
+import card49 from "/images/49.jpg"
+import card50 from "/images/50.jpg"
+import card51 from "/images/51.jpg"
+import card52 from "/images/52.jpg"
+import card53 from "/images/53.jpg"
+import card54 from "/images/54.jpg"
+import card55 from "/images/55.jpg"
+import card56 from "/images/56.jpg"
+import card57 from "/images/57.jpg"
+import card58 from "/images/58.jpg"
+import card59 from "/images/59.jpg"
+import card60 from "/images/60.jpg"
+import card61 from "/images/61.jpg"
+import card62 from "/images/62.jpg"
+import card63 from "/images/63.jpg"
+import card64 from "/images/64.jpg"
+import card65 from "/images/65.jpg"
+import card66 from "/images/66.jpg"
+import card67 from "/images/67.jpg"
+import card68 from "/images/68.jpg"
+import card69 from "/images/69.jpg"
+import card70 from "/images/70.jpg"
+import card71 from "/images/71.jpg"
+import card72 from "/images/72.jpg"
+import card73 from "/images/73.jpg"
+import card74 from "/images/74.jpg"
+import card75 from "/images/75.jpg"
+import card76 from "/images/76.jpg"
+import card77 from "/images/77.jpg"
+import card78 from "/images/78.jpg"
+import card79 from "/images/79.jpg"
+import card80 from "/images/80.jpg"
+import card81 from "/images/81.jpg"
+import card82 from "/images/82.jpg"
+import card83 from "/images/83.jpg"
+import card84 from "/images/84.jpg"
+import card85 from "/images/85.jpg"
+import card86 from "/images/86.jpg"
+import card87 from "/images/87.jpg"
+import card88 from "/images/88.jpg"
+import card89 from "/images/89.jpg"
+import card90 from "/images/90.jpg"
+import card91 from "/images/91.jpg"
+import card92 from "/images/92.jpg"
+import card93 from "/images/93.jpg"
+import card94 from "/images/94.jpg"
+import card95 from "/images/95.jpg"
+import card96 from "/images/96.jpg"
+import card97 from "/images/97.jpg"
+import card98 from "/images/98.jpg"
+import card99 from "/images/99.jpg"
+import card100 from "/images/100.jpg"
+import card101 from "/images/101.jpg"
+import card102 from "/images/102.jpg"
+import card103 from "/images/103.jpg"
+import card104 from "/images/104.jpg"
 
 export const cards: CardType[] = [
     {
         id: 1,
         lives: 1,
-        image: "/images/1.jpg"
+        image: card1
     },
     {
         id: 2,
         lives: 1,
-        image: "/images/2.jpg"
+        image: card2
     },
     {
         id: 3,
         lives: 1,
-        image: "/images/3.jpg"
+        image: card3
     },
     {
         id: 4,
         lives: 1,
-        image: "/images/4.jpg"
+        image: card4
     },
     {
         id: 5,
         lives: 1,
-        image: "/images/5.jpg"
+        image: card5
     },
     {
         id: 6,
         lives: 1,
-        image: "/images/6.jpg"
+        image: card6
     },
     {
         id: 7,
         lives: 1,
-        image: "/images/7.jpg"
+        image: card7
     },
     {
         id: 8,
         lives: 1,
-        image: "/images/8.jpg"
+        image: card8
     },
     {
         id: 9,
         lives: 1,
-        image: "/images/9.jpg"
+        image: card9
     },
     {
         id: 10,
         lives: 3,
-        image: "/images/10.jpg"
+        image: card10
     },
     {
         id: 11,
         lives: 5,
-        image: "/images/11.jpg"
+        image: card11
     },
     {
         id: 12,
         lives: 1,
-        image: "/images/12.jpg"
+        image: card12
     },
     {
         id: 13,
         lives: 1,
-        image: "/images/13.jpg"
+        image: card13
     },
     {
         id: 14,
         lives: 1,
-        image: "/images/14.jpg"
+        image: card14
     },
     {
         id: 15,
         lives: 2,
-        image: "/images/15.jpg"
+        image: card15
     },
     {
         id: 16,
         lives: 1,
-        image: "/images/16.jpg"
+        image: card16
     },
     {
         id: 17,
         lives: 1,
-        image: "/images/17.jpg"
+        image: card17
     },
     {
         id: 18,
         lives: 1,
-        image: "/images/18.jpg"
+        image: card18
     },
     {
         id: 19,
         lives: 1,
-        image: "/images/19.jpg"
+        image: card19
     },
     {
         id: 20,
         lives: 3,
-        image: "/images/20.jpg"
+        image: card20
     },
     {
         id: 21,
         lives: 1,
-        image: "/images/21.jpg"
+        image: card21
     },
     {
         id: 22,
         lives: 5,
-        image: "/images/22.jpg"
+        image: card22
     },
     {
         id: 23,
         lives: 1,
-        image: "/images/23.jpg"
+        image: card23
     },
     {
         id: 24,
         lives: 1,
-        image: "/images/24.jpg"
+        image: card24
     },
     {
         id: 25,
         lives: 2,
-        image: "/images/25.jpg"
+        image: card25
     },
     {
         id: 26,
         lives: 1,
-        image: "/images/26.jpg"
+        image: card26
     },
     {
         id: 27,
         lives: 1,
-        image: "/images/27.jpg"
+        image: card27
     },
     {
         id: 28,
         lives: 1,
-        image: "/images/28.jpg"
+        image: card28
     },
     {
         id: 29,
         lives: 1,
-        image: "/images/29.jpg"
+        image: card29
     },
     {
         id: 30,
         lives: 3,
-        image: "/images/30.jpg"
+        image: card30
     },
     {
         id: 31,
         lives: 1,
-        image: "/images/31.jpg"
+        image: card31
     },
     {
         id: 32,
         lives: 1,
-        image: "/images/32.jpg"
+        image: card32
     },
     {
         id: 33,
         lives: 5,
-        image: "/images/33.jpg"
+        image: card33
     },
     {
         id: 34,
         lives: 1,
-        image: "/images/34.jpg"
+        image: card34
     },
     {
         id: 35,
         lives: 2,
-        image: "/images/35.jpg"
+        image: card35
     },
     {
         id: 36,
         lives: 1,
-        image: "/images/36.jpg"
+        image: card36
     },
     {
         id: 37,
         lives: 1,
-        image: "/images/37.jpg"
+        image: card37
     },
     {
         id: 38,
         lives: 1,
-        image: "/images/38.jpg"
+        image: card38
     },
     {
         id: 39,
         lives: 1,
-        image: "/images/39.jpg"
+        image: card39
     },
     {
         id: 40,
         lives: 3,
-        image: "/images/40.jpg"
+        image: card40
     },
     {
         id: 41,
         lives: 1,
-        image: "/images/41.jpg"
+        image: card41
     },
     {
         id: 42,
         lives: 1,
-        image: "/images/42.jpg"
+        image: card42
     },
     {
         id: 43,
         lives: 1,
-        image: "/images/43.jpg"
+        image: card43
     },
     {
         id: 44,
         lives: 5,
-        image: "/images/44.jpg"
+        image: card44
     },
     {
         id: 45,
         lives: 2,
-        image: "/images/45.jpg"
+        image: card45
     },
     {
         id: 46,
         lives: 1,
-        image: "/images/46.jpg"
+        image: card46
     },
     {
         id: 47,
         lives: 1,
-        image: "/images/47.jpg"
+        image: card47
     },
     {
         id: 48,
         lives: 1,
-        image: "/images/48.jpg"
+        image: card48
     },
     {
         id: 49,
         lives: 1,
-        image: "/images/49.jpg"
+        image: card49
     },
     {
         id: 50,
         lives: 3,
-        image: "/images/50.jpg"
+        image: card50
     },
     {
         id: 51,
         lives: 1,
-        image: "/images/51.jpg"
+        image: card51
     },
     {
         id: 52,
         lives: 1,
-        image: "/images/52.jpg"
+        image: card52
     },
     {
         id: 53,
         lives: 1,
-        image: "/images/53.jpg"
+        image: card53
     },
     {
         id: 54,
         lives: 1,
-        image: "/images/54.jpg"
+        image: card54
     },
     {
         id: 55,
         lives: 7,
-        image: "/images/55.jpg"
+        image: card55
     },
     {
         id: 56,
         lives: 1,
-        image: "/images/56.jpg"
+        image: card56
     },
     {
         id: 57,
         lives: 1,
-        image: "/images/57.jpg"
+        image: card57
     },
     {
         id: 58,
         lives: 1,
-        image: "/images/58.jpg"
+        image: card58
     },
     {
         id: 59,
         lives: 1,
-        image: "/images/59.jpg"
+        image: card59
     },
     {
         id: 60,
         lives: 3,
-        image: "/images/60.jpg"
+        image: card60
     },
     {
         id: 61,
         lives: 1,
-        image: "/images/61.jpg"
+        image: card61
     },
     {
         id: 62,
         lives: 1,
-        image: "/images/62.jpg"
+        image: card62
     },
     {
         id: 63,
         lives: 1,
-        image: "/images/63.jpg"
+        image: card63
     },
     {
         id: 64,
         lives: 1,
-        image: "/images/64.jpg"
+        image: card64
     },
     {
         id: 65,
         lives: 2,
-        image: "/images/65.jpg"
+        image: card65
     },
     {
         id: 66,
         lives: 5,
-        image: "/images/66.jpg"
+        image: card66
     },
     {
         id: 67,
         lives: 1,
-        image: "/images/67.jpg"
+        image: card67
     },
     {
         id: 68,
         lives: 1,
-        image: "/images/68.jpg"
+        image: card68
     },
     {
         id: 69,
         lives: 1,
-        image: "/images/69.jpg"
+        image: card69
     },
     {
         id: 70,
         lives: 3,
-        image: "/images/70.jpg"
+        image: card70
     },
     {
         id: 71,
         lives: 1,
-        image: "/images/71.jpg"
+        image: card71
     },
     {
         id: 72,
         lives: 1,
-        image: "/images/72.jpg"
+        image: card72
     },
     {
         id: 73,
         lives: 1,
-        image: "/images/73.jpg"
+        image: card73
     },
     {
         id: 74,
         lives: 1,
-        image: "/images/74.jpg"
+        image: card74
     },
     {
         id: 75,
         lives: 2,
-        image: "/images/75.jpg"
+        image: card75
     },
     {
         id: 76,
         lives: 1,
-        image: "/images/76.jpg"
+        image: card76
     },
     {
         id: 77,
         lives: 5,
-        image: "/images/77.jpg"
+        image: card77
     },
     {
         id: 78,
         lives: 1,
-        image: "/images/78.jpg"
+        image: card78
     },
     {
         id: 79,
         lives: 1,
-        image: "/images/79.jpg"
+        image: card79
     },
     {
         id: 80,
         lives: 3,
-        image: "/images/80.jpg"
+        image: card80
     },
     {
         id: 81,
         lives: 1,
-        image: "/images/81.jpg"
+        image: card81
     },
     {
         id: 82,
         lives: 1,
-        image: "/images/82.jpg"
+        image: card82
     },
     {
         id: 83,
         lives: 1,
-        image: "/images/83.jpg"
+        image: card83
     },
     {
         id: 84,
         lives: 1,
-        image: "/images/84.jpg"
+        image: card84
     },
     {
         id: 85,
         lives: 2,
-        image: "/images/85.jpg"
+        image: card85
     },
     {
         id: 86,
         lives: 1,
-        image: "/images/86.jpg"
+        image: card86
     },
     {
         id: 87,
         lives: 1,
-        image: "/images/87.jpg"
+        image: card87
     },
     {
         id: 88,
         lives: 5,
-        image: "/images/88.jpg"
+        image: card88
     },
     {
         id: 89,
         lives: 1,
-        image: "/images/89.jpg"
+        image: card89
     },
     {
         id: 90,
         lives: 3,
-        image: "/images/90.jpg"
+        image: card90
     },
     {
         id: 91,
         lives: 1,
-        image: "/images/91.jpg"
+        image: card91
     },
     {
         id: 92,
         lives: 1,
-        image: "/images/92.jpg"
+        image: card92
     },
     {
         id: 93,
         lives: 1,
-        image: "/images/93.jpg"
+        image: card93
     },
     {
         id: 94,
         lives: 1,
-        image: "/images/94.jpg"
+        image: card94
     },
     {
         id: 95,
         lives: 2,
-        image: "/images/95.jpg"
+        image: card95
     },
     {
         id: 96,
         lives: 1,
-        image: "/images/96.jpg"
+        image: card96
     },
     {
         id: 97,
         lives: 1,
-        image: "/images/97.jpg"
+        image: card97
     },
     {
         id: 98,
         lives: 1,
-        image: "/images/98.jpg"
+        image: card98
     },
     {
         id: 99,
         lives: 5,
-        image: "/images/99.jpg"
+        image: card99
     },
     {
         id: 100,
         lives: 3,
-        image: "/images/100.jpg"
+        image: card100
     },
     {
         id: 101,
         lives: 1,
-        image: "/images/101.jpg"
+        image: card101
     },
     {
         id: 102,
         lives: 1,
-        image: "/images/102.jpg"
+        image: card102
     },
     {
         id: 103,
         lives: 1,
-        image: "/images/103.jpg"
+        image: card103
     },
     {
         id: 104,
         lives: 1,
-        image: "/images/104.jpg"
+        image: card104
     }
 ]
